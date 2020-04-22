13bf4ccf0ab962733656b7dd0f1c2f49740d7cc5
Eric Mann <emann@mail.niagara.edu>
2020-04-22 11:44:24 -0400

    add template for quick C&P for C# files under the program class

---
+++ fileTemplate.cs
@@ -0,0 +1,22 @@
+// Credit for this trick goes to Austin "Goose" Martin and 
+// this StackOverflow answer: https://stackoverflow.com/a/56909397
+
+/*
+  Empty file that should be used as a template to create CS files
+  that will talk to the program.cs file's Main function.
+  There, it can be called as 'ch.functionName();'
+  Note: The only thing that should change in this template is the functionName()
+*/
+
+using System;
+
+namespace LearningCS
+{
+    public partial class Chapter
+    {
+        public void functionName()
+        {
+
+        }
+    }
+}
